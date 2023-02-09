d96288ea238347917bab4b6124720b4e4cddf5e9
Mhd Ali Houria <alihoria123456@gmail.com>
2023-02-09 23:39:35 +0300

    Used ENV key instead of Exposing API KEY

--- src/Components/Weather.jsx
+++ src/Components/Weather.jsx
@@ -3,11 +3,10 @@ import React, { useEffect, useState } from "react";
 export const Weather = () => {
   const [weatherInfo, setWeatherInfo] = useState();
 
-  console.log(weatherInfo);
   useEffect(() => {
     const fetchWeatherData = async () => {
       const response = await fetch(
-        "http://api.openweathermap.org/data/2.5/weather?q=damascus&appid=0c6d4434045c81dc7d93e37e8fae1062&lat=33.513805&lon=36.276527"
+        `http://api.openweathermap.org/data/2.5/weather?q=damascus&appid=${import.meta.env.VITE_API_KEY}`
       );
       const data = await response.json();
       setWeatherInfo({
