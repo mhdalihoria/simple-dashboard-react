4653d10f4bc2d3f25bd85a7d00b7b9cfd03af4d0
Mhd Ali Houria <alihoria123456@gmail.com>
2023-02-10 12:12:24 +0300

    adding logout logic

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useContext } from "react";
+import React, { useState, useContext, useEffect } from "react";
 import logo from "../imgs/logo.png";
 import defultUsr from "../imgs/default-usr.png";
 import { Link } from "react-router-dom";
@@ -6,7 +6,8 @@ import { ContextObj } from "../Context";
 
 export default function Navbar() {
   const [showSettings, setShowSettings] = useState(false);
-  const { user } = useContext(ContextObj);
+  const [shouldLogout, setShouldLogout] = useState(false);
+  const { user, setUser } = useContext(ContextObj);
 
   const clickHandler = () => {
     setShowSettings((prevShowSettings) => {
@@ -14,30 +15,48 @@ export default function Navbar() {
     });
   };
 
+  useEffect(() => {
+    if (shouldLogout) {
+      setUser({
+        usrName: "",
+        email: "",
+        pwd: "",
+        pwd2: "",
+        imgLnk: "",
+        age: "",
+      });
+      localStorage.removeItem("user")
+    }
+  }, [shouldLogout]);
+
   return (
     <nav>
       <div className="left">
         <img src={logo} />
       </div>
       <div className="right">
-        <img src={user.imgLnk !== "" ? user.imgLnk : defultUsr} onClick={clickHandler} />
+        <img
+          src={user.imgLnk !== "" ? user.imgLnk : defultUsr}
+          onClick={clickHandler}
+        />
+        {showSettings && (
+          <div className="user-settings">
+            {user.usrName === "" ? (
+              <Link to="/login" className="login">
+                Login
+              </Link>
+            ) : (
+              <>
+                <Link to="/login" className="profile">
+                  Profile
+                </Link>
+
+                <span className="profile" onClick={() => setShouldLogout(true)}>Logout</span>
+              </>
+            )}
+          </div>
+        )}
       </div>
-      {showSettings && (
-        <div className="user-settings">
-          {user.usrName === "" ? (
-            <span>
-              <Link to="/login">Login</Link>
-            </span>
-          ) : (
-            <>
-              <span>
-                <Link to="/login">Profile</Link>
-              </span>
-              <span>Logout</span>
-            </>
-          )}
-        </div>
-      )}
     </nav>
   );
 }
